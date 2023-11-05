b114b44756e4b444626ccca564109d34af33cc9f
Matt Eland <matt.eland@gmail.com>
2023-11-05 16:09:31 -0500

    Ch 2 code progress

---
+++ Chapter2/Program.cs
@@ -0,0 +1,9 @@
+﻿using Azure;
+using Azure.AI.Vision.Common;
+using Azure.AI.Vision.ImageAnalysis;
+
+string endpoint = "https://YourUrl.cognitiveservices.azure.com/";
+string key = "YourKey";
+
+AzureKeyCredential credentials = new(key);
+VisionServiceOptions visionOptions = new(endpoint, credentials);
